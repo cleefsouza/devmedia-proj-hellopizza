4d55a389b8ed2b87b6650665424c6a1f47896cdc
cleefsouza <a.cleef.souza@gmail.com>
2019-01-21 03:33:47 -0300

    validações botão

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-// variaveis
-var inpQtd = document.getElementById("qtd");
-var btnIncrementa = document.querySelector("#btn-incrementa");
-var btnDecrementa = document.querySelector("#btn-decrementa");
+/*
+*  botão incrementa
+*/
+var btnIncrementa = document.querySelectorAll('.btn-incrementa');
 
 // ao cliclar no botão +
-btnIncrementa.addEventListener('click', incrementa);
+for (let btn of btnIncrementa) {
+    btn.addEventListener('click', incrementa);
+
+    // incrementar quantidade de pizza
+    function incrementa() {
+        var item = btn.closest('.item');
+
+        var inpQtd = item.querySelector('.quantidade');
+        inpQtd.value++;
+
+        adicionaAoTotal(pegarPrecoItem(item));
+    }
+}
+
+/*
+*  botão decrementa
+*/
+var btnDecrementa = document.querySelectorAll('.btn-decrementa');
 
 // ao cliclar no botão -
-btnDecrementa.addEventListener('click', decrementa);
+for (let btn of btnDecrementa) {
+    btn.addEventListener('click', decrementa);
 
-// incrementar quantidade de pizza
-function incrementa() {
-    inpQtd.value++;
-    var item = btnIncrementa.closest('.item');
-    adicionaAoTotal(pegarPrecoItem(item));
+    // decrementar quantidade de pizza
+    function decrementa() {
+        var item = btn.closest('.item');
 
+        var inpQtd = item.querySelector('.quantidade');
+        if (inpQtd.value > 0) {
+            inpQtd.value--;
+            adicionaAoTotal(-pegarPrecoItem(item));
+        } else {
+            console.log('não à pizza adicionada');
+        }
+    }
 }
 
-// decrementar quantidade de pizza
-function decrementa() {
-    inpQtd.value--;
-    var item = btnDecrementa.closest('.item');
-    removerDoTotal(pegarPrecoItem(item));
-}
+/*
+* funções auxiliares
+*/
 
 // pegar preço do item
 function pegarPrecoItem(item) {
@@ -36,8 +57,3 @@ function adicionaAoTotal(valor) {
     total.textContent = Number(total.textContent) + valor;
 }
 
-// remover valor do total
-function removerDoTotal(valor) {
-    var total = document.querySelector('#total');
-    total.textContent = Number(total.textContent) - valor;
-}
